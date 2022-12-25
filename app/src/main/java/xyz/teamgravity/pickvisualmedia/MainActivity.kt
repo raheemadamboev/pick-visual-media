4bb458214d1b179f670fba98e8a68a0001fa75a5
raheemadamboev <vividricemaryandmax@gmail.com>
2022-12-25 16:19:20 +0500

    build the app

--- app/src/main/java/xyz/teamgravity/pickvisualmedia/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/pickvisualmedia/MainActivity.kt
@@ -1,12 +1,28 @@
 package xyz.teamgravity.pickvisualmedia
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import xyz.teamgravity.pickvisualmedia.ui.theme.PickVisualMediaTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,6 +34,57 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    var selectedImageUri by remember { mutableStateOf<Uri?>(null) }
+                    var selectedImageUris by remember { mutableStateOf(emptyList<Uri>()) }
+
+                    val singlePhotoPickerLauncher = rememberLauncherForActivityResult(
+                        contract = ActivityResultContracts.PickVisualMedia(),
+                        onResult = { selectedImageUri = it }
+                    )
+                    val multiplePhotoPickerLauncher = rememberLauncherForActivityResult(
+                        contract = ActivityResultContracts.PickMultipleVisualMedia(),
+                        onResult = { selectedImageUris = it }
+                    )
+
+                    LazyColumn(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        item {
+                            Row(
+                                horizontalArrangement = Arrangement.SpaceAround,
+                                modifier = Modifier.fillMaxWidth(),
+                            ) {
+                                Spacer(modifier = Modifier.width(16.dp))
+                                Button(onClick = { singlePhotoPickerLauncher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)) }) {
+                                    Text(text = stringResource(id = R.string.pick_one_photo))
+                                }
+                                Spacer(modifier = Modifier.width(16.dp))
+                                Button(onClick = { multiplePhotoPickerLauncher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)) }) {
+                                    Text(text = stringResource(id = R.string.pick_multiple_photo))
+                                }
+                                Spacer(modifier = Modifier.width(16.dp))
+                            }
+                            if (selectedImageUri != null) {
+                                Spacer(modifier = Modifier.height(10.dp))
+                                AsyncImage(
+                                    model = selectedImageUri,
+                                    contentDescription = null,
+                                    contentScale = ContentScale.Crop,
+                                    modifier = Modifier.fillMaxWidth()
+                                )
+                            }
+                        }
+
+                        items(selectedImageUris) { uri ->
+                            Spacer(modifier = Modifier.height(10.dp))
+                            AsyncImage(
+                                model = uri,
+                                contentDescription = null,
+                                contentScale = ContentScale.Crop,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                        }
+                    }
                 }
             }
         }
